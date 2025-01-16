f8ca24a40370841cd8120afbf5dd92271b24c52b
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-16 15:55:55 -0500

    order manager and cli template

---
+++ order_manager.py
@@ -0,0 +1,3 @@
+class OrderManager:
+    def __init__(self):
+        self.order_books = {}
---
+++ order_matching_system.py
@@ -0,0 +1,9 @@
+from order_manager import OrderManager
+
+
+def main():
+    manager = OrderManager()
+
+
+if __name__ == "__main__":
+    main()
