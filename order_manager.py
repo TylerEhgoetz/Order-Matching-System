725461b78408ed7ecb3fa5a54863716ca7cd8b65
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-16 17:06:40 -0500

    simple CLI setup

--- order_manager.py
+++ order_manager.py
@@ -1,3 +1,12 @@
 class OrderManager:
     def __init__(self):
         self.order_books = {}
+
+    def submit_order(self):
+        pass
+
+    def display_order_book(self):
+        pass
+
+    def display_trades(self):
+        pass
--- order_matching_system.py
+++ order_matching_system.py
@@ -3,6 +3,32 @@ from order_manager import OrderManager
 
 def main():
     manager = OrderManager()
+    while True:
+        print("Simple Order Matching System")
+        print("1. Submit Order")
+        print("2. Display Order Book")
+        print("3. Display Trades")
+        print("4. Exit")
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            symbol = input("Enter security symbol: ").upper()
+            order_type = input("Enter order type (BUY/SELL): ").upper()
+            quantity = int(input("Enter quantity: "))
+            price = float(input("Enter price: "))
+            manager.submit_order(symbol, order_type, quantity, price)
+        elif choice == "2":
+            symbol = input("Enter security symbol to view Order Book: ").upper()
+            manager.display_order_book(symbol)
+        elif choice == "3":
+            symbol = input("Enter security symbol to view Trades: ").upper()
+            manager.display_trades(symbol)
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    print("Exiting Order Matching System, Thank you!")
 
 
 if __name__ == "__main__":
