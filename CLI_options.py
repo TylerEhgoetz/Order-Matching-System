5d53e21cb2f02c9a5ebc54694bade72ed18d8ebb
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-16 17:17:50 -0500

    added enums

---
+++ CLI_options.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class CLIOptions(Enum):
+    SUBMIT_ORDER = 1
+    DISPLAY_ORDER_BOOK = 2
+    DISPLAY_TRADES = 3
+    EXIT = 4
--- order_matching_system.py
+++ order_matching_system.py
@@ -1,29 +1,44 @@
 from order_manager import OrderManager
+from CLI_options import CLIOptions
+from order_type import OrderType
+
+
+def handle_submit_order(manager: OrderManager):
+    symbol = input("Enter security symbol: ").upper()
+    while True:
+        order_type = input("Enter order type (BUY/SELL): ").upper() == "BUY"
+        if order_type == OrderType.BUY.name:
+            order_type = OrderType.BUY.value
+            break
+        elif order_type == OrderType.SELL.name:
+            order_type = OrderType.SELL.value
+            break
+        else:
+            print("Invalid order type. Please try again.")
+    quantity = int(input("Enter quantity: "))
+    price = float(input("Enter price: "))
+    manager.submit_order(symbol, order_type, quantity, price)
 
 
 def main():
     manager = OrderManager()
     while True:
         print("Simple Order Matching System")
-        print("1. Submit Order")
-        print("2. Display Order Book")
-        print("3. Display Trades")
-        print("4. Exit")
+        print(f"{CLIOptions.SUBMIT_ORDER.value}. Submit Order")
+        print(f"{CLIOptions.DISPLAY_ORDER_BOOK.value}. Display Order Book")
+        print(f"{CLIOptions.DISPLAY_TRADES.value}. Display Trades")
+        print(f"{CLIOptions.EXIT.value}. Exit")
         choice = input("Enter choice: ")
 
-        if choice == "1":
-            symbol = input("Enter security symbol: ").upper()
-            order_type = input("Enter order type (BUY/SELL): ").upper()
-            quantity = int(input("Enter quantity: "))
-            price = float(input("Enter price: "))
-            manager.submit_order(symbol, order_type, quantity, price)
-        elif choice == "2":
+        if choice == str(CLIOptions.SUBMIT_ORDER.value):
+            handle_submit_order(manager)
+        elif choice == str(CLIOptions.DISPLAY_ORDER_BOOK.value):
             symbol = input("Enter security symbol to view Order Book: ").upper()
             manager.display_order_book(symbol)
-        elif choice == "3":
+        elif choice == str(CLIOptions.DISPLAY_TRADES.value):
             symbol = input("Enter security symbol to view Trades: ").upper()
             manager.display_trades(symbol)
-        elif choice == "4":
+        elif choice == str(CLIOptions.EXIT.value):
             break
         else:
             print("Invalid choice. Please try again.")
