463d6a9f335c947273e7fb858402d4ae40e62163
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-16 17:18:10 -0500

    updated order constructor

--- order.py
+++ order.py
@@ -1,12 +1,13 @@
 from datetime import datetime
+from order_type import OrderType
 
 
 class Order:
-    def __init__(self, symbol: str, quantity: int, price: float, is_buy_order: bool):
+    def __init__(self, symbol: str, quantity: int, price: float, order_type: OrderType):
         self.symbol = symbol
         self.quantity = quantity
         self.price = price
-        self.is_buy_order = is_buy_order
+        self.is_buy_order = order_type
         self.timestamp = datetime.now()
         self.order_id = self.generate_order_id()
 
