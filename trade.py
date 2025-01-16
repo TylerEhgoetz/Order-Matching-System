117b9bee6d4aca10dffa115fdcfc18eab1e99c97
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-16 15:47:33 -0500

    a basic trade class

---
+++ trade.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+class Trade:
+    def __init__(
+        self,
+        buy_order_id: str,
+        sell_order_id: str,
+        symbol: str,
+        quantity: int,
+        price: float,
+    ):
+        self.buy_order_id = buy_order_id
+        self.sell_order_id = sell_order_id
+        self.symbol = symbol
+        self.quantity = quantity
+        self.price = price
+        self.timestamp = datetime.now()
+
+    def __repr__(self):
+        return f"Trade({self.buy_order_id}, {self.sell_order_id}, {self.symbol}, {self.quantity}, {self.price}, {self.timestamp})"
